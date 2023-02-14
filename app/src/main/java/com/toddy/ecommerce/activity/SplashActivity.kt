678842a0c0174a3a06a43fd4d89ac35197bc2a3b
biduzilla <rlyowlly19@gmail.com>
2023-02-14 10:57:11 -0300

    layout login

--- app/src/main/java/com/toddy/ecommerce/activity/SplashActivity.kt
+++ app/src/main/java/com/toddy/ecommerce/activity/SplashActivity.kt
@@ -1,31 +1,25 @@
 package com.toddy.ecommerce.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.toddy.ecommerce.MainActivity
 import com.toddy.ecommerce.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-    }
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) hideSystemUI()
-    }
-
-
-    private fun hideSystemUI() {
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        val handler = Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            finish()
+            startActivity(Intent(this,MainActivity::class.java))
+        }, 3000)
     }
 }
