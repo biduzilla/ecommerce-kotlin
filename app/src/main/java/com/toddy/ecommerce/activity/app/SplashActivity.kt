0b6536bf72883f03d6f447554526f939bcd79027
biduzilla <rlyowlly19@gmail.com>
2023-02-15 14:47:19 -0300

    bottom nav view

--- app/src/main/java/com/toddy/ecommerce/activity/app/SplashActivity.kt
+++ app/src/main/java/com/toddy/ecommerce/activity/app/SplashActivity.kt
@@ -1,17 +1,12 @@
-package com.toddy.ecommerce.activity
+package com.toddy.ecommerce.activity.app
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
-import com.toddy.ecommerce.MainActivity
+import com.toddy.ecommerce.activity.usuario.MainActivityUsuario
 import com.toddy.ecommerce.R
-import com.toddy.ecommerce.auth.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +15,7 @@ class SplashActivity : AppCompatActivity() {
 
         val handler = Handler(Looper.getMainLooper()).postDelayed(Runnable {
             finish()
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivityUsuario::class.java))
         }, 3000)
     }
 }
--- app/src/main/java/com/toddy/ecommerce/auth/RecuperaContaActivity.kt
+++ app/src/main/java/com/toddy/ecommerce/auth/RecuperaContaActivity.kt
@@ -2,8 +2,12 @@ package com.toddy.ecommerce.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.toddy.ecommerce.R
 import com.toddy.ecommerce.databinding.ActivityRecuperaContaBinding
+import com.toddy.ecommerce.helper.FireBaseHelper
 
 class RecuperaContaActivity : AppCompatActivity() {
 
@@ -19,5 +23,32 @@ class RecuperaContaActivity : AppCompatActivity() {
 
     private fun configClicks() {
         binding.btnVoltar.ibVoltar.setOnClickListener { finish() }
+    }
+
+    private fun validaDados() {
+        val email: String = binding.edtEmail.text.toString()
+
+        if (email.isEmpty()) {
+            binding.edtEmail.requestFocus()
+            binding.edtEmail.error = "Campo Obrigatório"
+        } else {
+            binding.progressBar.visibility = View.VISIBLE
+            enviarEmail(email)
+        }
+    }
+
+    private fun enviarEmail(email: String) {
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Email de recuperação enviado", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    FireBaseHelper.validaErros(it.exception!!.message!!),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        binding.progressBar.visibility = View.GONE
     }
 }
