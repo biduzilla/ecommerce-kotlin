85d5903192eb3c033d622fedaf67a96a4aa10409
biduzilla <rlyowlly19@gmail.com>
2023-02-16 17:59:33 -0300

    mudança de cor botão pressionado

--- app/src/main/java/com/toddy/ecommerce/fragment/loja/LojaCategoriaFragment.kt
+++ app/src/main/java/com/toddy/ecommerce/fragment/loja/LojaCategoriaFragment.kt
@@ -1,19 +1,23 @@
 package com.toddy.ecommerce.fragment.loja
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.toddy.ecommerce.R
+import com.toddy.ecommerce.databinding.DialogFormCategoriaBinding
 import com.toddy.ecommerce.databinding.FragmentLojaCategoriaBinding
+
 
 class LojaCategoriaFragment : Fragment() {
 
     private var _binding: FragmentLojaCategoriaBinding? = null
     private val binding get() = _binding!!
-    private lateinit var alertDialog:AlertDialog
+    private lateinit var alertDialog: AlertDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,15 +32,37 @@ class LojaCategoriaFragment : Fragment() {
         configClicks()
     }
 
-    private fun configClicks(){
-        binding.btnAddCategoria.setOnClickListener { showDialog() }
+    private fun configClicks() {
+        binding.btnAddCategoria.setOnClickListener {
+            showDialog()
+        }
+
+//        binding.btnAddCategoria.setOnTouchListener(object : OnTouchListener{
+//            override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+//
+//                when(p1!!.action){
+//                    MotionEvent.ACTION_UP -> {
+//                        binding.btnAddCategoria.animate().scaleX(1.0f).scaleY(1.0f).duration = 0
+//                    }
+//                    MotionEvent.ACTION_DOWN -> {
+//                        binding.btnAddCategoria.animate().scaleX(0.9f).scaleY(0.9f).duration = 0
+//                    }
+//                }
+//                return true
+//            }
+//        })
     }
 
-    private fun showDialog(){
-        val alert = AlertDialog.Builder(requireContext(), R.style.CustomAlertDialog)
-        val view = layoutInflater.inflate(R.layout.dialog_form_categoria, null)
 
-        alert.setView(view)
+    private fun showDialog() {
+        val alert = AlertDialog.Builder(requireContext(), R.style.CustomAlertDialog)
+        val categoriaBinding = DialogFormCategoriaBinding.inflate(LayoutInflater.from(context))
+
+        categoriaBinding.btnFechar.setOnClickListener { alertDialog.dismiss() }
+
+        categoriaBinding.btnSalvar.setOnClickListener { alertDialog.dismiss() }
+
+        alert.setView(categoriaBinding.root)
 
         alertDialog = alert.create()
         alertDialog.show()
