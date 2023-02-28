c5a90ec8ca1a70aff3f0731f932ed241dd816e15
biduzilla <rlyowlly19@gmail.com>
2023-02-28 10:44:25 -0300

    layout configurações da loja

---
+++ app/src/main/java/com/toddy/ecommerce/activity/loja/LojaConfigActivity.kt
@@ -0,0 +1,12 @@
+package com.toddy.ecommerce.activity.loja
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.toddy.ecommerce.R
+
+class LojaConfigActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loja_config)
+    }
+}
